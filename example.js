7ce33d50c4f9741efb6268cf5a06c839baec547c
Matías Lescano <mjlescano@protonmail.com>
2017-06-25 20:18:51 -0300

    fix example

--- example.js
+++ example.js
@@ -4,20 +4,34 @@ const config = require('./lib/config')
 const notifier = require('.')
 
 config.set({
-  'mongoUrl': 'mongodb://localhost/DemocracyOS-dev-singleForum'
+  mongoUrl: 'mongodb://localhost/DemocracyOS-dev'
 })
 
 notifier.start()
   .then(() => {
-    notifier.agenda.now('welcome-email', {
+    console.log('Executing job welcome-email')
+
+    return notifier.now('welcome-email', {
       to: 'matias@democracyos.org',
       validateUrl: 'https://app.democracyos.org'
-    }, () => {
-      console.log('User notified!')
-      process.exit(0)
     })
   })
+  .then(() => {
+    return new Promise((resolve, reject) => {
+      notifier.agenda.once('success:welcome-email', (job) => {
+        resolve(job)
+      })
+
+      notifier.agenda.once('fail:welcome-email', (err, job) => {
+        reject(err)
+      })
+    })
+  })
+  .then((job) => {
+    console.log('User notified!', job.attrs)
+    process.exit(0)
+  })
   .catch((err) => {
-    console.error(err)
+    console.error('Error!', err)
     process.exit(1)
   })
