544c5cd73e06f3857088b43979c1a288b234accb
Pablo Mendez <mendez.developer@gmail.com>
2018-09-16 20:46:29 -0300

    Added simple tests

--- server.js
+++ server.js
@@ -6,3 +6,5 @@ const port = process.env.PORT || 3000;
 const server = http.createServer(app);
 
 server.listen(port, () => console.log('Server listening on port:', port));
+
+module.exports = server;
