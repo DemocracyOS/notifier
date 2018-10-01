25baa234f14938fabf820947b22b8af653800a89
Pablo Mendez <mendez.developer@gmail.com>
2018-10-01 19:55:50 -0300

    deleted "old" folder

--- old/lib/utils/index.js
+++
@@ -1,6 +0,0 @@
-module.exports.emailAddress = function emailAddress (user) {
-  return {
-    name: user.lastName ? user.firstName + ' ' + user.lastName : user.firstName,
-    address: user.email
-  }
-}
