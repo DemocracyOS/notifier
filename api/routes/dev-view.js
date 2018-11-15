ef59d66e81b6f287a505fc6cd26ca1ea7fff044d
franciclo <francisco@democracyos.io>
2018-11-15 15:35:53 -0300

    update comment templates and dev views

--- api/routes/dev-view.js
+++ api/routes/dev-view.js
@@ -6,7 +6,19 @@ router.get('/:type', (req, res, next) => {
     if (!fs.existsSync(`templates/${req.params.type}.js`)) return next()
 
     const template = require(`../../templates/${req.params.type}`);
-    res.send(template(req.query))
+    let props
+
+    try {
+        props = JSON.parse(req.query.props)
+    } catch (err) {
+        console.log(err)
+    }
+
+    console.log('props')
+    console.log(props)
+    console.log('-----------------------------')
+
+    res.send(template(props))
 });
 
 module.exports = router;
--- templates/comment-liked.js
+++ templates/comment-liked.js
@@ -1,6 +1,4 @@
 const React = require('react');
-const Fragment = React.Fragment
-const ReactDom = require('react-dom/server');
 const { Email, Item, Span, A, renderEmail, Box, Image } = require ('react-html-email');
 const { ORGANIZATION_NAME, ORGANIZATION_URL } = process.env;
 const spanStyles = {
@@ -26,20 +24,21 @@ const mailStyle = {
 const HomeLink = () => <A href={process.env.ORGANIZATION_URL}>{process.env.ORGANIZATION_NAME}</A>
 
 const CommentLiked = (props) => {
+
   return (
     <Email>
-      <Box align="center" style={{width: 700, height: 136, border: 'solid 1px #e9e9e9'}}>
+      <Box align="center" style={{width: '100%', height: 136}}>
         <Item align="center" style={{border: 'solid 1px #e9e9e9'}}>
           <Image src='https://www.hcdn.gob.ar/system/modules/ar.gob.hcdn.frontend/resources/img/logo-hcdn-vertical.jpg' align="center" style={{width: 130, height: 98, margin: 25}} />
         </Item>
 
         <Item align="center" style={{display: "block", marginTop: 25, marginLeft: 191, marginRight: 191}}>
-          <Span {...pStyle}>Elaboración de <Span style={{fontWeight: 'bold', fontSize: 20}}>Propuestas de Ley</Span></Span>
+          <Span {...pStyle}>Portal de <Span style={{fontWeight: 'bold', fontSize: 20}}>Co-creación Legislativa</Span></Span>
         </Item>
 
-        <Item style={{display: "block", marginTop: 25, margin: 57}}>
+        <Item style={{display: "block", maxWidth: 700, margin: '40px auto 0'}}>
           <Span {...spanStyles}>
-            Hola {props.participant.name},
+            Hola {props.participant.fullname},
           </Span>
           <Box style={{marginTop: 30}}>
             <Item>
@@ -49,37 +48,40 @@ const CommentLiked = (props) => {
                   ? 'La diputada '
                   : props.accountable.gender === 'Masculino'
                     ? 'El diputado '
-                    : 'El/la diputado/a ') + props.accountable.fullname
+                    : 'El/la diputado/a ') + props.accountable.fullname + ' '
                 }
-                apoyó el comentario que realizó en la propuesta de { props.title && props.title.toLowerCase() } en <HomeLink />. Ingrese en <HomeLink /> para conocer y comentar en otros proyectos.
+                apoyó el comentario que realizó en la propuesta de { props.title && props.title.toLowerCase() } en <HomeLink />.
               </Span>
             </Item>
           </Box>
           <Box style={{marginTop: 20}}>
             <Item>
-              <Span {...mailStyle}>Este fue tu comentario marcado como:</Span>
+              <Span {...mailStyle}>Este fue tu comentario:</Span>
             </Item>
           </Box>
         </Item>
 
-        <Box align="center" style={{border: 'solid 1px #dae1e7', width: 585, height:130, marginLeft: 56}}>
+        <Box align="center" style={{border: 'solid 1px #dae1e7', width: 585, height:130, marginTop: 20}}>
           <Item>
             <Box style={{width: 50, height: 140, backgroundColor: '#f2f5f8'}}>
               <Item>
-                <Box style={{marginBottom: 91, marginLeft: 14}}>
+                <Box style={{marginBottom: 77, marginLeft: 14}}>
                   <Item>
                     <Image src="https://complejoteatral.blob.core.windows.net/assets/like.png" style={{width: 24.7, height: 24.7}}/>
                   </Item>
                 </Box>
               </Item>
             </Box>
-
-            <Box align="center" style={{marginLeft: 121, marginTop: 16}}>
+            <Box align="center" style={{marginLeft: 80, marginTop: 16}}>
               <Item >
-                <Span>{props.participant.name}</Span>
+                <Span style={{ fontSize: 14, fontWeight: 'bold', color: '#2c4c61'}}>
+                  {props.participant.fullname}
+                </Span>
               </Item>
               <Item>
-                <Span>{props.participant.occupation}</Span>
+                <Span style={{ fontSize: 12, color: '#5c97bc' }}>
+                  {props.participant.occupation && props.participant.occupation.toUpperCase()}
+                </Span>
               </Item>
             </Box>
             <Box align="center" style={{marginLeft: 80, marginTop: 20, marginBottom: 22}}>
@@ -90,7 +92,7 @@ const CommentLiked = (props) => {
           </Item>
         </Box>
 
-        <Box align="center" style={{width: 700, height: 126, marginTop: 49, borderBottom: 'solid 1px #e9e9e9', backgroundColor: '#5c98bd'}}>
+        <Box align="center" style={{width: '100%', height: 126, marginTop: 49, borderBottom: 'solid 1px #e9e9e9', backgroundColor: '#5c98bd'}}>
           <Item>
             <Box align="center">
               <Item align="center" style={{width: 535, height: 40, margin: 82}}>
